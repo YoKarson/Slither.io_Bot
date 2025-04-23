37dde7de0bea04e32949fd0e1a11fb1a9f4d1112
YoKarson <lantk991@gmail.com>
2025-04-22 23:46:13 -0700

    creating starting point for code. Set up selenium  for automated actions by bot

---
+++ main.py
@@ -0,0 +1,13 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+import time
+
+service = Service(executable_path="chromedriver.exe")
+driver = webdriver.Chrome(service=service)
+
+driver.get("https://slither.io")
+
+time.sleep(10)
+
+driver.quit()
